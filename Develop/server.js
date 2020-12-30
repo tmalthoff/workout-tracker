52d93b0e02eb1160d7567f91004443c797ee4d31
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-29 20:50:48 -0600

    Add option for static middleware

--- Develop/server.js
+++ Develop/server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'))
 
 app.use('/api', require('./routes'))
 
-app.use(express.static("public"))
+app.use(express.static("public", { extensions: ['html']}))
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
