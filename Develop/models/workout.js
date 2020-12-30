5a3568a936d518f9bf7b0595829bfaed66ffe727
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-29 19:54:33 -0600

    Make some fields required

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,12 +2,23 @@ let mongoose = require("mongoose");
 
 let workoutSchema = new mongoose.Schema({
     day: {
-        type: Date, required: true
+        type: Date, default: () => new Date()
     },
     exercises: [{
-        type: String,
-        name: String,
-        duration: Number,
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+        },
+            duration: {
+            type: Number,
+            required: "Enter an exercise duration in minutes"
+        },
         weight: Number,
         reps: Number,
         sets: Number
