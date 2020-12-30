24b696ac87fe30883cca5cbb30152f73dd7150f0
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-29 21:19:44 -0600

    Moved logger up

--- Develop/server.js
+++ Develop/server.js
@@ -10,11 +10,11 @@ mongoose.connect("mongodb://localhost/workout", {
   useUnifiedTopology: true
 });
 
+app.use(morgan('dev'))
+
 // only required to receive web forms posted directly from a browser
 //app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
-app.use(morgan('dev'))
 
 app.use('/api', require('./routes'))
 
